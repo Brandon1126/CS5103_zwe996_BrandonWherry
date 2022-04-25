088ecfc1d01452a9f24d4f6d6797bded59c064a7
Brandon W <bwh1126@gmail.com>
2022-04-25 12:41:39 -0700

    testing suites to automate the testing of several combinations of inputs

--- testsuite_normal.sh
+++ testsuite_normal.sh
@@ -3,8 +3,6 @@
 TEST_NUMS=('test0.txt' 'test1.txt' 'test2.txt' 'test3.txt' 'test4.txt' \
 'test5.txt' 'test6.txt' 'test7.txt' 'test8.txt' 'test9.txt')
 
-# This line is to remove old results
-rm -f normal_tests_results.txt
 
 # Double forloop to execute all possible combinations of inputs. 100 different tests will be generated
 # and redirected to normal_tests_results.txt
@@ -12,12 +10,14 @@ for test_num_fileA in "${TEST_NUMS[@]}"
 do
   for test_num_fileB in "${TEST_NUMS[@]}"
   do
-    echo "@@@@@@ A = $test_num_fileA @@@@@@@@@ B = $test_num_fileB @@@@@@" >> normal_tests_results.txt
-    echo "Performing +, -, and * on A and B" >> normal_tests_results.txt
-    ./BigNumberMath add testnums_normal/"$test_num_fileA" testnums_normal/"$test_num_fileB" &>> normal_tests_results.txt
-    ./BigNumberMath sub testnums_normal/"$test_num_fileA" testnums_normal/"$test_num_fileB" &>> normal_tests_results.txt
-    ./BigNumberMath mul testnums_normal/"$test_num_fileA" testnums_normal/"$test_num_fileB" &>> normal_tests_results.txt
-    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> normal_tests_results.txt
+    echo "@@@@@@ A = $test_num_fileA @@@@@@@@@ B = $test_num_fileB @@@@@@"
+    echo "Performing +, -, and * on A and B"
+    ./BigNumberMath add testnums_normal/"$test_num_fileA" testnums_normal/"$test_num_fileB"
+    ./BigNumberMath sub testnums_normal/"$test_num_fileA" testnums_normal/"$test_num_fileB"
+    ./BigNumberMath mul testnums_normal/"$test_num_fileA" testnums_normal/"$test_num_fileB"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
   done
 done
 
+echo " "
+echo "Testing completed"
--- testsuite_unusual.sh
+++ testsuite_unusual.sh
@@ -3,8 +3,6 @@
 TEST_NUMS=('test0.txt' 'test1.txt' 'test2.txt' 'test3.txt' 'test4.txt' \
 'test5.txt' 'test6.txt' 'test7.txt' 'test8.txt' 'test9.txt')
 
-# This line is to remove old results
-rm -f unusual_tests_results.txt
 
 # Double forloop to execute all possible combinations of inputs. 100 different tests will be generated
 # and redirected to unusual_tests_results.txt
@@ -12,11 +10,33 @@ for test_num_fileA in "${TEST_NUMS[@]}"
 do
   for test_num_fileB in "${TEST_NUMS[@]}"
   do
-    echo "@@@@@@ A = $test_num_fileA @@@@@@@@@ B = $test_num_fileB @@@@@@" >> unusual_tests_results.txt
-    echo "Performing +, -, and * on A and B" >> normal_tests_results.txt
-    ./BigNumberMath add testnums_unusual/"$test_num_fileA" testnums_unusual/"$test_num_fileB" &>> unusual_tests_results.txt
-    ./BigNumberMath sub testnums_unusual/"$test_num_fileA" testnums_unusual/"$test_num_fileB" &>> unusual_tests_results.txt
-    ./BigNumberMath mul testnums_unusual/"$test_num_fileA" testnums_unusual/"$test_num_fileB" &>> unusual_tests_results.txt
-    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> unusual_tests_results.txt
+    echo "@@@@@@ A = $test_num_fileA @@@@@@@@@ B = $test_num_fileB @@@@@@"
+    echo "Performing +, -, and * on A and B"
+    ./BigNumberMath add testnums_unusual/"$test_num_fileA" testnums_unusual/"$test_num_fileB"
+    ./BigNumberMath sub testnums_unusual/"$test_num_fileA" testnums_unusual/"$test_num_fileB"
+    ./BigNumberMath mul testnums_unusual/"$test_num_fileA" testnums_unusual/"$test_num_fileB"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
   done
 done
+
+# Other testing
+echo " "
+echo "Invalid program inputs"
+echo " "
+echo " "
+
+# No arguments provided
+echo "Trying command ./BigNumberMath with no arguments"
+./BigNumberMath
+echo " "
+
+# Wrong operation provided - "blah"
+echo "Trying command ./BigNumberMath blah A.txt B.txt"
+./BigNumberMath blah testnums_unusual/test0.txt testnums_unusual/test1.txt
+echo " "
+
+# Missing input one file
+echo "Trying command ./BigNumberMath add A.txt"
+./BigNumberMath add testnums_unusual/test0.txt
+echo " "
+echo "Testing completed"
